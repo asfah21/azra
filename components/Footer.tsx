3d917b456bac80cfdb902ee6dd1a867dcb8294c5
Muh Al Asfahani <asfah21@gmail.com>
2025-06-28 17:39:20 +0800

    feat: add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,16 +4,16 @@ import { Link } from "@heroui/link";
 
 export default function Footer() {
   return (
-    <footer className="flex items-center w-full border-t p-5 text-default-600 justify-center">
+    <footer className="flex items-center w-full border-t border-default-200 p-5 bg-default-50 justify-center">
       <Link
-        isExternal
-        className="flex items-center gap-1 text-current text-xs md:text-base"
-        href="/about"
-        title="AZRA - About Us"
+        href="/"
+        className="flex items-center gap-1 text-default-600 text-small"
+        color="foreground"
+        underline="none"
       >
-        <span>&copy; {new Date().getFullYear()} </span>
-        <p className="text-primary font-semibold">&nbsp;AZRA</p>
-        <span>&nbsp;- All rights reserved</span>
+        <span>&copy; {new Date().getFullYear()}</span>
+        <span className="bg-gradient-to-r from-blue-600 to-green-600 bg-clip-text text-transparent font-semibold hover:from-green-600 hover:to-blue-600 transition-all duration-300">Azvan</span>
+        <span>IT. All rights reserved.</span>
       </Link>
     </footer>
   );
