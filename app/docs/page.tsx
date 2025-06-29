153b5801246423c08c6b8f3ef85615de7a5cee9c
Muh Al Asfahani <asfah21@gmail.com>
2025-06-29 14:56:05 +0800

    page v2

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function DocsPage() {
   return (
     <div>
-      <h1 className={title()}>Docs</h1>
+      <h1 className={title()}>Docs Page</h1>
       <div className="container mx-auto px-4 mt-10" />
     </div>
   );
